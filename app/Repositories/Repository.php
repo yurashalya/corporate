f2c415f2eac3c07c67ce4dd60ea48121ea92928c
yurashalya <stallker9696@gmail.com>
2017-05-16 18:30:02 +0300

    создание отдельного материала блога, а именно дочерние классы

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -9,13 +9,18 @@ abstract class Repository {
     protected $model = FALSE;
 
 
-    public function get($select = '*',$take = FALSE, $pagination = FALSE) {
+    public function get($select = '*',$take = FALSE,$pagination = FALSE, $where = FALSE) {
 
         $builder = $this->model->select($select);
 
         if($take) {
             $builder->take($take);
         }
+
+        if($where) {
+            $builder->where($where[0],$where[1]);
+        }
+
 
         if($pagination) {
             return $this->check($builder->paginate(Config::get('settings.paginate')));
@@ -44,6 +49,12 @@ abstract class Repository {
 
         return $result;
 
+    }
+
+    public function one($alias,$attr = array()) {
+        $result = $this->model->where('alias',$alias)->first();
+
+        return $result;
     }
 
 }
