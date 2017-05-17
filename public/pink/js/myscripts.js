a84a25e8c2400bb6a032278b34d089fa109955d3
yurashalya <stallker9696@gmail.com>
2017-05-17 20:00:43 +0300

    Доработка отображения для добавленого коментария

--- public/pink/js/myscripts.js
+++ public/pink/js/myscripts.js
@@ -27,9 +27,45 @@ jQuery(document).ready(function($) {
                 type:'POST',
                 datatype:'JSON',
                 success: function(html) {
+                    if(html.error) {
+                        $('.wrap_result').css('color','red').append('<br /><strond>Ошибка: </strong>' + html.error.join('<br />'));
+                        $('.wrap_result').delay(2000).fadeOut(500);
+                    }
+                    else if(html.success) {
+                        $('.wrap_result')
+                            .append('<br /><strong>Сохранено!</strong>')
+                            .delay(2000)
+                            .fadeOut(500,function() {
+
+                                if(html.data.parent_id > 0) {
+                                    comParent.parents('div#respond').prev().after('<ul class="children">' + html.comment + '</ul>');
+                                }
+                                else {
+                                    if($.contains('#comments','ol.commentlist')) {
+                                        $('ol.commentlist').append(html.comment);
+                                    }
+                                    else {
+
+                                        $('#respond').before('<ol class="commentlist group">' + html.comment + '</ol>');
+
+                                    }
+                                }
+
+
+
+
+                                $('#cancel-comment-reply-link').click();
+                            })
+
+                    }
+
 
                 },
                 error:function() {
+                    $('.wrap_result').css('color','red').append('<br /><strond>Ошибка: </strong>');
+                    $('.wrap_result').delay(2000).fadeOut(500, function() {
+                        $('#cancel-comment-reply-link').click();
+                    });
 
                 }
 
